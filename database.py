7fb568abf2c69f40a99b34fba1fedc1e514a53ed
Tehs <tehseinf@gmail.com>
2023-02-23 09:50:38 +0000

    add

--- database.py
+++ database.py
@@ -9,21 +9,21 @@ name VARCHAR(128),
 position_lat FLOAT,
 position_lng FLOAT,
 PRIMARY KEY (number)
-);
-CREATE TABLE availability (
+);"""
+pql="""CREATE TABLE availability (
 number INTEGER NOT NULL,
 last_update DATETIME NOT NULL,
 available_bikes INTEGER,
 available_bike_stands INTEGER,
 status VARCHAR(128),
 PRIMARY KEY (number, last_update)
-);
+);"""
 
-"""
 import mysql.connector
 
 engine = mysql.connector.connect(host="softwaredb.ce0otalnccc9.eu-west-1.rds.amazonaws.com",user="soft",password="password",database="dublinbikes")
 mycursor = engine.cursor()
 mycursor.execute(sql)
+mycursor.execute(pql)
 print(engine)
 
