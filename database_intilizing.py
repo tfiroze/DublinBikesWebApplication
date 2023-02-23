89a337dab383855ad3acb43fea907246b731db2a
Tehs <tehseinf@gmail.com>
2023-02-23 09:53:18 +0000

    added intilization db quety file

--- database_intilizing.py
+++ database_intilizing.py
@@ -9,21 +9,22 @@ name VARCHAR(128),
 position_lat FLOAT,
 position_lng FLOAT,
 PRIMARY KEY (number)
-);"""
-pql="""CREATE TABLE availability (
+);
+CREATE TABLE availability (
 number INTEGER NOT NULL,
 last_update DATETIME NOT NULL,
 available_bikes INTEGER,
 available_bike_stands INTEGER,
 status VARCHAR(128),
 PRIMARY KEY (number, last_update)
-);"""
+);
 
+"""
 import mysql.connector
 
 engine = mysql.connector.connect(host="softwaredb.ce0otalnccc9.eu-west-1.rds.amazonaws.com",user="soft",password="password",database="dublinbikes")
 mycursor = engine.cursor()
-
-mycursor.execute(pql)
+mycursor.execute("CREATE DATABASE dublinbikes")
+mycursor.execute(sql)
 print(engine)
 
