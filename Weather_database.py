964a97288bd12c221cf3a04da66c45dd552daedb
Tehs <tehseinf@gmail.com>
2023-03-02 12:07:45 +0000

    add

--- Weather_database.py
+++ Weather_database.py
@@ -1,12 +1,13 @@
 sql = """CREATE TABLE weather (
+number INTEGER NOT NULL,
 time DATETIME NOT NULL,
 temperature INTEGER,
-wind speed INTEGER,
-wind direction INTEGER,
-weather code INT,
-precipitation sum FLOAT,
-rain sum FLOAT,
-precipitation probability INT,
+wind_speed INTEGER,
+wind_direction INTEGER,
+weather_code INTEGER,
+precipitation_sum FLOAT,
+rain_sum FLOAT,
+precipitation_probability INTEGER,
 PRIMARY KEY (number)
 );
 
@@ -15,7 +16,6 @@ import mysql.connector
 
 engine = mysql.connector.connect(host="softwaredb.ce0otalnccc9.eu-west-1.rds.amazonaws.com",user="soft",password="password",database="dublinbikes")
 mycursor = engine.cursor()
-mycursor.execute("CREATE DATABASE dublinbikes")
 mycursor.execute(sql)
 print(engine)
 
