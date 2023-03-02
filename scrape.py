9f5bf7bb01e18b2029493c9127c71e3da1c79f5e
Tehs <tehseinf@gmail.com>
2023-03-02 12:29:16 +0000

    add

--- scrape.py
+++ scrape.py
@@ -47,14 +47,12 @@ NAME="Dublin" # name of contract
 STATIONS="https://api.jcdecaux.com/vls/v1/stations?"
 APIKEY = "772474ead9d6527be32fcb2c7b1d631d990fa561"
 def main():
-    while True:
-        try:
-            r = requests.get(STATIONS,params={"apiKey": APIKEY, "contract": NAME})
-            data=json.loads(r.text)
-            print(data[0])
-            availability(data)
-            time.sleep(5*60)
-        except:
-            print(traceback.format_exc())
-            return
+    try:
+        r = requests.get(STATIONS,params={"apiKey": APIKEY, "contract": NAME})
+        data=json.loads(r.text)
+        availability(data)
+        time.sleep(5*60)
+    except:
+        print(traceback.format_exc())
+        return
 main()
