cccf48fbae23081fcbf23d6d4911c4e0df23bd71
Tehs <tehseinf@gmail.com>
2023-04-09 16:44:58 +0100

    added markers

--- static/script.js
+++ static/script.js
@@ -1,22 +1,41 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Dublin
-  const Dublin = { lat: 53.350140, lng: -6.266155 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
-    center: Dublin,
-    disableDefaultUI: true,
+function addMarkers(stations) {
+  for (const station of stations) {
+  console.log(station);
+  var marker = new google.maps.Marker({
+  position: {
+  lat: station.position_lat,
+  lng: station.position_lng,
+  },
+  map: map,
+  title: station.name,
+  station_number: station.number,
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: Dublin,
-    map: map,
+  }
+  }
+  function getStations() {
+  fetch("http://127.0.0.1:5000/stations")
+  .then((response) => response.json())
+  .then((data) => {
+  console.log("fetch response", typeof data);
+  addMarkers(data);
   });
-}
-
-window.initMap = initMap;
-
+  }
+  // Initialize and add the map
+  function initMap() {
+  const dublin = {lat: 53.3498, lng: -6.2603};
+  // The map, centered at Dublin
+  map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 13,
+  center: dublin,
+  });
+  // const marker = new google.maps.Marker({
+  // position: dublin,
+  // map: map,
+  // });
+  getStations();
+  }
+  var map = null;
+  window.initMap = initMap;
 window.onload = function(){
   const sidebar = document.getElementById("sidebar")
   const toggle = document.getElementById("toggle")
