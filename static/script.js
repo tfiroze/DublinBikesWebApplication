a35e91e73d92419333b3084e7b5145f309061087
Mugesh <mushi025@gmail.com>
2023-04-21 15:42:05 +0100

    JS

--- static/script.js
+++ static/script.js
@@ -2,13 +2,11 @@ var markers = [];
 var map;
 var mc;
 var directionService;
-const directionRenderers = [null, null, null, null];
-const dublin = {lat: 53.3498, lng: -6.2603};
-function addMarkerClickListener(marker, contentString) {
+var directionRenderer;
+const dublin = {lat: 53.3498, lng: -6.2603};function addMarkerClickListener(marker, contentString, occupancy) {
   var infowindow = new google.maps.InfoWindow({
     content: contentString,
   });
-
   marker.addListener("mouseover", () => {
     infowindow.open(map, marker);
   });
@@ -16,10 +14,57 @@ function addMarkerClickListener(marker, contentString) {
   marker.addListener("mouseout", () => {
     infowindow.close();
   });
+  
+  marker.addListener("click", () => {
+    drawChart(occupancy)
+  });
+
 }
+
+function drawChart(occupancy) {
+  var occupancyTable = google.visualization.arrayToDataTable(occupancy);
+
+  var options = {
+    width: "100%",
+    height: "100%",
+    legend: { position: "top", maxLines: 3 },
+    bar: { groupWidth: "75%" },
+    isStacked: true,
+    colors: ["#8A00C2", "#65FE08"],
+  };
+
+  var graphdiv = document.getElementById("OccGraph");
+  var chart = new google.visualization.ColumnChart(graphdiv);
+  chart.draw(occupancyTable, options);
+
+  graphdiv.style.transform = "translateY(-100%)";
+  window.addEventListener("resize", function () {
+    chart.draw(occupancyTable, options);
+  });
+
+  // Add a close button for the graph
+  var closeButton = document.createElement("button");
+  closeButton.innerHTML = "&times;";
+  closeButton.style.position = "absolute";
+  closeButton.style.top = "10px";
+  closeButton.style.right = "10px";
+  closeButton.style.zIndex = 100;
+  closeButton.style.background = "none";
+  closeButton.style.border = "none";
+  closeButton.style.fontSize = "24px";
+  closeButton.style.cursor = "pointer";
+  closeButton.addEventListener("click", function () {
+    graphdiv.style.transform = "translateY(0%)";
+    closeButton.remove(); // Remove the close button when the graph is closed
+  });
+
+  graphdiv.appendChild(closeButton);
+}
+
 //console.log(station_data)
 
-function addMarkers(station_data,availabilityData){
+
+function addMarkers(station_data,availabilityData,history){
   //console.log(availabilityData)
   for (const station of station_data) {
     var marker = new google.maps.Marker({
@@ -35,6 +80,71 @@ function addMarkers(station_data,availabilityData){
         scaledSize: new google.maps.Size(35, 35),
       },
     });
+
+    var number = station.number;
+    var stathist = history[number]; //get the array with just the station number
+
+    var recent = null; //will hold most recent time
+    var recentstat = null; //will hold dictionary containing most recent time
+    if (stathist) {
+      stathist.forEach(stat => { //short for station history don't @ me
+      const time = stat.last_update; //using last_update becase it's easier to compare and its not like the data will be different without an update
+      if (!recent || time > recent) {
+        recent = time;
+        recentstat = stat;
+      }})
+    }
+
+    //create dictionary holding all updates within each hour
+    //for each hour, go through and average the number of bikes and stands
+    //return an array of arrays that's like [time, average bikes, average stands]
+    //Make graph
+    var hourstat= {}
+    var occupancy=[["Time ","Available Bikes ", "Available Stands " ]]
+    var date
+    var hour
+    //console.log(stathist)
+    if (stathist) {
+
+    for (c = 0; c < stathist.length; ++c) {
+      date=new Date (stathist[c].time)
+      hour=date.getHours()
+      if (hour in hourstat){
+        hourstat[hour].push(stathist[c])
+      }
+      else{
+        hourstat[hour]=[stathist[c]]
+      }
+    }
+  }
+    var len = Object.keys(hourstat).length
+    for(var key in hourstat){
+      var avgbike=0
+      var avgstand=0
+      for (i = 0; i < hourstat[key].length; ++i){
+        avgbike+=hourstat[key][i]["available_bikes"]
+        avgstand+=hourstat[key][i]["available_bike_stands"]
+      }
+
+      avgbike/=hourstat[key].length
+      avgstand/=hourstat[key].length
+      var timestr
+      if (key>12){
+        key=key-12
+        timestr=key.toString()+"pm"
+      }
+      else if (key==12){
+        timestr="12pm"
+      }
+      else if (key==0){
+        timestr="12am"
+      }
+      else{
+        timestr=key.toString()+"am"
+      }
+      occupancy.push([timestr, avgbike, avgstand])
+    }
+    //console.log(occupancy)
     let availability;
     for (const a of availabilityData) {
       if (parseInt(a.number) === parseInt(station.number)) {
@@ -47,6 +157,7 @@ function addMarkers(station_data,availabilityData){
 
     // Check if availability data exists for the station
     if (availability) {
+      
       var contentString = `
         <div>
           <h3>${station.name}</h3>
@@ -56,10 +167,11 @@ function addMarkers(station_data,availabilityData){
           <p>Last Update: ${availability.time}</p>
         </div>
       `;
-      addMarkerClickListener(marker, contentString);
+      addMarkerClickListener(marker, contentString, occupancy);
     } else {
       console.warn(`No availability data found for station ${station.number}`);
     }
+    
     markers.push(marker);
   }
 }
@@ -72,10 +184,14 @@ async function getStations() {
     station_data = await data;
     const availabilityResponse = await fetch("http://127.0.0.1:5000/availability");
     availabilityData = await availabilityResponse.json();
-
+    const history = await fetch("http://127.0.0.1:5000/history");
+    historydata = await history.json();
+    console.log(station_data);
+    // populateStationDropdowns(station_data);
     // Pass the availabilityData to the addMarkers function
     //addMarkers(station_data, availabilityData);
-    setInterval(addMarkers(station_data, availabilityData), 300000);
+    addMarkers(station_data, availabilityData,historydata);
+     // Make sure to add this line
   }
 
   // Initialize and add the map
@@ -134,12 +250,18 @@ function searchStations() {
     const startStation = start[1][0].station_number;
     const endStation = end[1][0].station_number;
     const dateTime = document.getElementById("datetime-input").value;
-
+    
     console.log("Start Station:", startStation);
     console.log("End Station:", endStation);
     console.log("Date and Time:", dateTime);
 
     const dateObj = new Date(dateTime);
+    
+    if (dateTime == "") {
+      dateObj = new Date();
+    } 
+
+
     const hour = dateObj.getHours();
     const day = dateObj.getDay();
     const minute = dateObj.getMinutes();
@@ -223,7 +345,7 @@ async function fetch_weather(){
   
 fetch_weather()
 
-async function nearestStation(place, drIndex){
+async function nearestStation(place){
   var shortestDistance = Infinity;
   var shortestDistanceMarker;
   
@@ -241,7 +363,7 @@ async function nearestStation(place, drIndex){
   return [shortestDistanceMarker, direction_result[0]/60, direction_result[1]/1000];
 }
 
-async function findDirection(start, end, display, drIndex, mode="WALKING"){
+async function findDirection(start, end, display, mode="WALKING"){
 
   directionService = new google.maps.DirectionsService();
   directionRenderer = new google.maps.DirectionsRenderer({
@@ -392,16 +514,12 @@ window.onload = function(){
       directionRenderer = null;
     }
     start.push(await searchPlaces(startDest));
-    const nearest_station_marker = await nearestStation(start[0]);
+    var nearest_station_marker = await nearestStation(start[0]);
     start.push(nearest_station_marker);
     findDirection(start[0], nearest_station_marker[0], true);
   })
 
   endDest.addEventListener("click", async function() {
-    if(!start && directionRenderer) {
-      directionRenderer.setMap(null);
-      directionRenderer = null;
-    }
     end.push(await searchPlaces(endDest));
     var nearest_station_marker = await nearestStation(end[0]);
     end.push(nearest_station_marker);
